6a17485c8d11f37d5f49d8e539eb23ad1c59d007
Nipun Rajput <nipun.rajput6586s@gmail.com>
2026-01-05 18:12:39 +0000

    Commit 1

--- Readme.md
+++ Readme.md
@@ -1,1 +1,4 @@
 This Repo will contain TS/JS/Python contributions from 4 Dec 2026
+
+--First We shall do a Quick Overview of JavaScript fundamentals before deep diving into depth
+
---
+++ Sandbox/Test_01.js
@@ -0,0 +1,5 @@
+console.log(
+    "Hi There , We are in 2026, the goal for this repo is to get back on track with dev(derailed for last 6 months)"
+
+    //Console.log is function in javascript
+)
